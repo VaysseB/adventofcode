5997d2a173fbc8e546809cf5bfe66de6e6e7a2c5
vaysseb <bvayssegm@gmail.com>
2022-12-09 14:18:18 +0100

    feat: day 8 completed

--- day_08/lib.py
+++ day_08/lib.py
@@ -15,6 +15,16 @@ class Forest:
         id: str
         height: int
 
+    @dataclasses.dataclass()
+    class View:
+        left: int = 0
+        right: int = 0
+        up: int = 0
+        down: int = 0
+
+        def score(self) -> int:
+            return self.left * self.right * self.up * self.down
+
     _by_rows: tp.List[tp.List[Tree]]
     _by_columns: tp.List[tp.List[Tree]]
 
@@ -30,7 +40,7 @@ class Forest:
         return cls(by_rows, by_columns)
 
     @classmethod
-    def _look(cls, lines: tp.List[tp.List[Tree]], way: int) -> tp.Iterable[Tree]:
+    def _count_visible(cls, lines: tp.List[tp.List[Tree]], way: int) -> tp.Iterable[Tree]:
         assert way in [1, -1], "invalid parameter 'way'"
 
         for line in lines:
@@ -41,19 +51,56 @@ class Forest:
                 line = list(reversed(line))
 
             yield line[0]
+            tallest = line[0]
 
-            for prev, next in zip(line[:-1], line[1:]):
-                if prev.height > next.height:
-                    break
-                elif prev.height < next.height:
-                    yield next
+            for curr in line[1:]:
+                if tallest.height >= curr.height:
+                    continue
+
+                yield curr
+                tallest = curr
 
     def count_visible(self) -> int:
-        left_seen = self._look(self._by_rows, 1)
-        right_seen = self._look(self._by_rows, -1)
-        top_seen = self._look(self._by_columns, 1)
-        bottom_seen = self._look(self._by_columns, -1)
+        left_seen = self._count_visible(self._by_rows, 1)
+        right_seen = self._count_visible(self._by_rows, -1)
+        top_seen = self._count_visible(self._by_columns, 1)
+        bottom_seen = self._count_visible(self._by_columns, -1)
         return set(itertools.chain(left_seen, right_seen, top_seen, bottom_seen))
+
+    def scenic(self, target_x: int, target_y: int) -> View:
+        view = self.View()
+        target = self._by_rows[target_y][target_x]
+
+        for current in reversed(self._by_rows[target_y][:target_x]):
+            view.left += 1
+            if target.height <= current.height:
+                break
+
+        for current in self._by_rows[target_y][target_x+1:]:
+            view.right += 1
+            if target.height <= current.height:
+                break
+
+        for current in reversed(self._by_columns[target_x][:target_y]):
+            view.up += 1
+            if target.height <= current.height:
+                break
+
+        for current in self._by_columns[target_x][target_y+1:]:
+            view.down += 1
+            if target.height <= current.height:
+                break
+
+        return view
+
+    def best_scenic(self) -> View:
+        return max(
+            (score
+            for y, line in enumerate(self._by_rows[1:-1], start=1)
+            for x, _ in enumerate(line[1:-1], start=1)
+            for score in [self.scenic(x, y)]),
+            key=self.View.score
+        )
 
 
 def solve(input: io.TextIOBase):
@@ -67,10 +114,9 @@ def solve(input: io.TextIOBase):
         raw.append([int(x) for x in line])
 
     forest = Forest.from_raw(raw)
-    yield len(forest.count_visible()), None
 
-    if False:
-        yield
+    yield len(forest.count_visible()), None
+    yield forest.best_scenic().score(), None
 
 
 def solve_golf(input: io.TextIOBase):
