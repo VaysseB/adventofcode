24c2046d95efe80e0d34fc39985d42a4eeaf45ed
vaysseb <bvayssegm@gmail.com>
2022-12-13 11:24:54 +0100

    style: update day template

--- day_XX/lib.py
+++ day_XX/lib.py
@@ -2,13 +2,18 @@ import io
 
 
 def solve(inputs: tp.List[io.TextIOBase]):
-    for line in input.readlines():
-        line = line.rstrip("\n")
-        if not line:
-            break
+    answers = []
 
-    if False:
-        yield
+    for input in inputs:
+
+        for line in input.readlines():
+            line = line.rstrip("\n")
+            if not line:
+                break
+
+        answers.append(None)
+
+    yield answers[0], None
 
 
 def solve_golf(inputs: tp.List[io.TextIOBase]):
